31a63460dd1f424d117e75d4c24f33ba1ef91bb6
guha-rahul <52607971+guha-rahul@users.noreply.github.com>
2023-07-26 00:23:35 +0530

    Update buynft.js

--- scripts/buynft.js
+++ scripts/buynft.js
@@ -2,10 +2,14 @@ Contract_address = "0x";
 
 const ethers = require("hardhat");
 
-export const tokenbuy = async (from, to, token_id, value) => {
+export const tokenbuy = async (from, token_id, value) => {
   const MyToken = await ethers.getContractFactory("MyToken");
-  await MyToken.attach(Contract_address)._safeTransferFrom(from,to,token_id {
-    value: ethers.utils.parseEther(value),
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+// Prompt user for account connections
+await provider.send("eth_requestAccounts", []);
+const signer = provider.getSigner();
+  await MyToken.attach(Contract_address)._safeTransferFrom(from,signer.address,token_id {
+    value: ethers.utils.parseEther(value),  // nft gets transfered from "from " address to signer.address
   }); ///value should be in string
   console.log(`The nft with ${token_id} is transfered from ${from} to ${to}`);
 };
