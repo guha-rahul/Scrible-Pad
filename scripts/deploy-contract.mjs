3c49a34ad91ea2ef5227385af6baf8ff08be6be8
guha-rahul <52607971+guha-rahul@users.noreply.github.com>
2023-07-25 23:57:34 +0530

    Update deploy-contract.mjs

--- scripts/deploy-contract.mjs
+++ scripts/deploy-contract.mjs
@@ -1,20 +1,19 @@
-import { storeAsset } from "./store-asset.mjs";
-import { mintNFT } from "./mint-nft.mjs";
+// import { storeAsset } from "./store-asset.mjs";
+// import { mintNFT } from "./mint-nft.mjs";
 async function deployContract() {
-  const ExampleNFT = await ethers.getContractFactory("ExampleNFT");
+  const ExampleNFT = await ethers.getContractFactory("bebrah ");
   const exampleNFT = await ExampleNFT.deploy();
   await exampleNFT.deployed();
 
-  // This solves the bug in Mumbai network where the contract address is not the real one
   const txHash = exampleNFT.deployTransaction.hash;
   const txReceipt = await ethers.provider.waitForTransaction(txHash);
 
   const contractAddress = txReceipt.contractAddress;
   console.log("Contract deployed to address:", contractAddress);
 
-  const metadata_url = await storeAsset();
-  console.log(metadata_url);
-  await mintNFT(contractAddress, metadata_url);
+  // const metadata_url = await storeAsset();
+  // console.log(metadata_url);
+  // await mintNFT(contractAddress, metadata_url);
 }
 
 deployContract()
