cb84dbabf809a72dbd52526601b2a38fdea8092a
guha-rahul <52607971+guha-rahul@users.noreply.github.com>
2023-07-26 00:21:01 +0530

    Update buynft.js

--- scripts/buynft.js
+++ scripts/buynft.js
@@ -2,10 +2,10 @@ Contract_address = "0x";
 
 const ethers = require("hardhat");
 
-export const tokenbuy = async (minter, amount) => {
+export const tokenbuy = async (from, to, token_id, value) => {
   const MyToken = await ethers.getContractFactory("MyToken");
-  await MyToken.attach(Contract_address).mint(minter, amount, {
-    value: ethers.utils.parseEther("0.1"),
-  });
-  console.log(`${amount} tokens are minted to ${minter}`);
+  await MyToken.attach(Contract_address)._safeTransferFrom(from,to,token_id {
+    value: ethers.utils.parseEther(value),
+  }); ///value should be in string
+  console.log(`The nft with ${token_id} is transfered from ${from} to ${to}`);
 };
